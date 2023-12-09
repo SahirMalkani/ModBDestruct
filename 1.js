dbbcb9c36cb3a55f119fd7796062675fe9e5581e
SahirMalkani <41510686+SahirMalkani@users.noreply.github.com>
2023-12-09 20:41:18 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,46 @@
+let sahir; 
+console.log(sahir)
+
+ sahir ="sahir1212"
+
+ var abc=`i am ${sahir}  `
+
+console.log(abc)
+
+ let obj={
+
+name:"your name",
+city:"your city"
+
+
+ }
+
+
+ var myobj=obj;
+
+ myobj.name="sahir"
+ myobj.city="khi"
+
+
+ console.log(myobj)
+ console.log(obj)
+
+
+ const {name:name1,city}=obj
+
+console.log(name1, city)
+
+
+
+
+let obj1=[0,-1,-2,5  
+]
+    
+    
+const [pehla,duja,teeja]=obj1
+
+console.log(duja)
+
+
+
+
